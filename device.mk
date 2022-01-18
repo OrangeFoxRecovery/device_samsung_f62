6ee8028639907e67c06af36e9638f0c68aab8747
Sushrut1101 <guptasushrut@gmail.com>
2022-01-18 17:01:43 +0530

    f62: Build Fastbootd Packages
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -18,3 +18,9 @@ LOCAL_PATH := device/samsung/f62
 
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+	fastbootd \
+	android.hardware.fastboot@1.0-impl-mock \
+	android.hardware.fastboot@1.0-impl-mock.recovery
